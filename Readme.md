64647fd43c2da801d85a70f73904a20c72aef456
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create the "select all" check box dynamically
+
+
+<p><strong>UPDATED:</strong><br /><br />Starting with version v2014 vol 1 (v14.1), this functionality is available out of the box:<br /><a href="https://www.devexpress.com/Support/Center/p/S173621">S173621: ASPxGridView - Add the built-in SelectAll CheckBox for Command Column</a><br /><br />Simply set the <strong>GridViewCommandColumn.SelectAllCheckboxMode</strong> property to <strong>GridViewSelectAllCheckBoxMode.AllPages</strong> to activate it. Please refer to the <a href="https://community.devexpress.com/blogs/aspnet/archive/2014/05/28/asp-net-gridview-select-all-rows-updated-coming-soon-in-v14-1.aspx">ASP.NET: GridView Select All Rows Updated</a> blog post and the <a href="http://demos.devexpress.com/ASPxGridViewDemos/Selection/AdvancedSelection.aspx">Select All Rows</a> demo for more information.<br /><br />This sample illustrates how to add the "select all" check box dynamically within the HeaderCaptionTemplateContainer of the command column using the ITemplate interface.</p>
+
+<br/>
+
+
---
+++ VB/WebApplication3/Default.aspx.vb
@@ -0,0 +1,85 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+
+Namespace WebApplication3
+
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Private check As New SelectAllCheckbox()
+		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxGridView1.DataSource = AccessDataSource1
+			ASPxGridView1.DataBind()
+
+		End Sub
+
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			If ASPxGridView1.Columns("Command") Is Nothing Then
+				AddColumn(ASPxGridView1, 0)
+			Else
+				ASPxGridView1.Columns("Command").HeaderCaptionTemplate = check
+			End If
+		End Sub
+
+		Private Sub AddColumn(ByVal grid As ASPxGridView, ByVal index As Integer)
+			Dim col As New GridViewCommandColumn()
+			col.Name = "Command"
+			col.ShowSelectCheckbox = True
+			col.HeaderCaptionTemplate = check
+			col.VisibleIndex = index
+
+			grid.Columns.Insert(index,col)
+
+		End Sub
+
+		Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+			Dim isSelected As Boolean = Convert.ToBoolean(e.Parameters)
+			If isSelected Then
+				CType(sender, ASPxGridView).Selection.SelectAll()
+			Else
+				CType(sender, ASPxGridView).Selection.UnselectAll()
+			End If
+			check.IsSelected = isSelected
+		End Sub
+
+
+
+	End Class
+
+	Friend Class SelectAllCheckbox
+		Implements ITemplate
+		Private _isSelected As Boolean
+		Public Property IsSelected() As Boolean
+			Get
+				Return _isSelected
+			End Get
+			Set(ByVal value As Boolean)
+				_isSelected = value
+			End Set
+		End Property
+		Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+			Dim box As New ASPxCheckBox()
+			box.ID = "ASPxCheckBox1"
+			box.Checked = _isSelected
+			box.ClientSideEvents.CheckedChanged = "function(s,e){grid.PerformCallback(s.GetChecked());}"
+			container.Controls.Add(box)
+		End Sub
+	End Class
+
+
+
+
+End Namespace
+
+
+
